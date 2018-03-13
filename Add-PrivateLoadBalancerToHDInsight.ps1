10715f1b9524e7c923113e86b30fb459f0debfca
Steven Judd <steven.judd@dvn.com>
2018-03-12 19:27:29 -0500

    Modified the Add-PrivateLoadBalancerToHDInsight function. See the NOTES help text block for details. Also, added a controller script to call the Add-PrivateLoadBalancerToHDInsight function.

--- Add-PrivateLoadBalancerToHDInsight.ps1
+++ Add-PrivateLoadBalancerToHDInsight.ps1
@@ -1,11 +1,4 @@
-$platform = [Environment]::OSVersion.Platform
-if ($platform -ne 'Win32NT') {
-    Import-Module AzureRM.Netcore
-}
-else {
-    Import-Module AzureRM
-}
-function Add-PrivateLoadBalancerToHDInsight {
+﻿function Add-PrivateLoadBalancerToHDInsight {
     <#
         .SYNOPSIS 
             Creates an internal load balancer for an HDInsight cluster
@@ -14,14 +7,42 @@ function Add-PrivateLoadBalancerToHDInsight {
             It does this by creating an internal load balancer and adding its address pool to the headnode network interfaces of 
             the specified cluster.
         .EXAMPLE
+            Add-PrivateLoadBalancerToHDInsight -Cluster (Get-AzureRmHDInsightCluster -ResourceGroupName HDIDev -ClusterName HDICluster1)
+
+            Make sure you are connected to Azure and are in the proper subscription for the
+            HDI cluster. Running this command will create an internal load balancer for the
+            HDICluster1 cluster in the HDIDev resource group.
+        .EXAMPLE
+            $cluster = Get-AzureRmHDInsightCluster -ResourceGroupName HDIDev -ClusterName HDICluster1
+            Add-PrivateLoadBalancerToHDInsight -Cluster $cluster -Verbose
+
+            Make sure you are connected to Azure and are in the proper subscription for the
+            HDI cluster. The first command will get the object for the HDInsight cluster
+            and store it in the $cluster variable. The second command will use this
+            variable as the Cluster parameter to create an internal load balancer for the
+            HDICluster1 cluster in the HDIDev resource group.
         .NOTES
-            VERSION: 0.1.0
+            VERSION: 0.1.1
             AUTHOR: Tyler Gregory <tdgregory@protonmail.com>
+            Modified by Steven Judd on 2018/03/12:
+                Added CmdletBinding for advanced functions
+                Moved Import-Module into the function definition
+                Added a check to ensure the AzureRM module is loaded
+                Converted the commands with many parameters to use parameter splatting
+                Switched commands that were using pipeline to pass objects to commands that use defined parameters
+                Added Verbose output to display the steps in the process as they occur
+                Added Examples to the help
+                Added a Warning notice that setting the load balancer backend address pool for the headnode network interfaces can take a while
+                Added the GitHub link to the help text
         .INPUTS
             Parameter 'Cluster' takes 'Microsoft.Azure.Commands.HDInsight.Models.AzureHDInsightCluster'
         .OUTPUTS
             Microsoft.Azure.Commands.Network.Models.PSLoadBalancer
+        .LINK
+            https://github.com/01100010011001010110010101110000/AzureRmUtilities
     #>
+    
+    [CmdletBinding()]
     Param(
         # HDInsight Cluster
         [Parameter(Mandatory = $true)]
@@ -29,35 +50,110 @@ function Add-PrivateLoadBalancerToHDInsight {
         $Cluster
     )
 
+<#
+    #the checks for the AzureRM module may be redundant since the script requires a AzureHDInsightCluster object as a parameter
+    if ([Environment]::OSVersion.Platform -eq 'Win32NT') {
+        $AzureRmModule = 'AzureRM'
+    }
+    else {
+        $AzureRmModule = 'AzureRM.Netcore'
+    }
+
+    Write-Verbose 'Ensure AzureRM module is loaded'
+    if (-not(Get-Module -Name $AzureRmModule -ErrorAction SilentlyContinue))
+    {
+        try
+        {
+            Import-Module -Name $AzureRmModule
+        }
+        catch
+        {
+            throw $_
+        }
+    }
+#>
+
+    Write-Verbose 'Get the Public IP Address object for the cluster'
     $publicIp = Get-AzureRmPublicIpAddress | Where-Object { $_.DnsSettings.DomainNameLabel -eq $Cluster.Name }
+    Write-Verbose 'Get the cluster ID for the cluster'
     $clusterId = $publicIp.Id.split('/')[-1].split('-')[-1]
 
-    $gatewayNics = Get-AzureRmNetworkInterface | Where-Object { $_.Name -like "*gateway*$clusterId" } # Ambari 443
-    $headNics = Get-AzureRmNetworkInterface | Where-Object { $_.Name -like "*headnode*$clusterId" } # Ambari 8080
+    Write-Verbose 'Get the gateway NICs, headnode NICs, subnet, and Virtual Network for the cluster'
+    $networkInterface = Get-AzureRmNetworkInterface
+    $gatewayNics = $networkInterface | Where-Object { $_.Name -like "*gateway*$clusterId" } # Ambari 443
+    $headNics = $networkInterface | Where-Object { $_.Name -like "*headnode*$clusterId" } # Ambari 8080
     $subnet = $headNics[0].IpConfigurations[0].Subnet
     $vNet = Get-AzureRmVirtualNetwork -ResourceGroupName $subnet.Id.split('/')[4] -Name $subnet.Id.split('/')[8]
 
+    Write-Verbose 'Create the Load Balancer Frontend IP Config for "hdi-private-gateway"'
     $frontendIPConfig = New-AzureRmLoadBalancerFrontendIpConfig -Subnet $subnet -Name 'hdi-private-gateway'
+    Write-Verbose 'Create the Load Balancer Backend Address Pool Config for "hdi-headnode-private-pool"'
     $headPool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name "hdi-headnode-private-pool"
-    $httpProbe = New-AzureRmLoadBalancerProbeConfig -Name "TCP" -Port 8080 -IntervalInSeconds 5 -ProbeCount 2 -Protocol Tcp
-    $httpRule = New-AzureRmLoadBalancerRuleConfig -Name "HTTP" -FrontendIpConfiguration $frontendIPConfig `
-        -BackendAddressPool $headPool -Probe $httpProbe -Protocol Tcp -FrontendPort 8080 -BackendPort 8080
 
-    $privateLb = New-AzureRmLoadBalancer -ResourceGroupName $vNet.ResourceGroupName -Name "$clusterId-private-lb" `
-        -BackendAddressPool $headPool -FrontendIpConfiguration $frontendIPConfig -Probe $httpProbe `
-        -Location $headNics[0].Location -LoadBalancingRule $httpRule
+    Write-Verbose 'Create Load Balancer Probe Configuration for HTTP'
+    $newLBProbeConfigParams = @{'Name' = 'TCP'
+                                'Port' = '8080'
+                                'IntervalInSeconds' = '5'
+                                'ProbeCount' = '2'
+                                'Protocol' =  'Tcp'}
+    $httpProbe = New-AzureRmLoadBalancerProbeConfig @newLBProbeConfigParams
 
-    $privateLb | Add-AzureRmLoadBalancerProbeConfig -Port 22 -Name 'SSH' -Protocol Tcp -IntervalInSeconds 5 -ProbeCount 2 | Set-AzureRmLoadBalancer | Out-Null
+    Write-Verbose 'Create Load Balancer Rule Configuration for HTTP'
+    $lbRuleParams = @{'Name' = 'HTTP'
+                      'FrontendIpConfiguration' = $frontendIPConfig
+                      'BackendAddressPool' = $headPool
+                      'Probe' = $httpProbe
+                      'Protocol' = 'Tcp'
+                      'FrontendPort' = '8080'
+                      'BackendPort' = '8080'}
+    $httpRule = New-AzureRmLoadBalancerRuleConfig @lbRuleParams
+
+    Write-Verbose "Create Load Balancer for $clusterId-private-lb"
+    $newLBParams = @{'ResourceGroupName' = $vNet.ResourceGroupName
+                     'Name' = "$clusterId-private-lb"
+                     'BackendAddressPool' = $headPool
+                     'FrontendIpConfiguration' = $frontendIPConfig
+                     'Probe' = $httpProbe
+                     'Location' = $headNics[0].Location
+                     'LoadBalancingRule' = $httpRule}
+    $privateLb = New-AzureRmLoadBalancer @newLBParams
+
+    Write-Verbose "Add probe configuration to allow SSH for $clusterId-private-lb"
+    $addLBProbeConfigParams = @{'LoadBalancer' = $privateLb
+                                'Port' = '22'
+                                'Name' = 'SSH'
+                                'Protocol' = 'Tcp'
+                                'IntervalInSeconds' = '5'
+                                'ProbeCount' = '2'}
+    $lbProbeConfig = Add-AzureRmLoadBalancerProbeConfig @addLBProbeConfigParams
+    Write-Verbose 'Set the Load Balancer to allow SSH probe'
+    $null = Set-AzureRmLoadBalancer -LoadBalancer $lbProbeConfig
+
+    Write-Verbose 'Get objects for the SSH probe and the hdi-headnode-private-pool Backend Address Pools'
     $sshProbe = $privateLb.Probes | Where-Object { $_.Name -eq 'SSH' }
     $headPoolObj = $privateLb.BackendAddressPools | Where-Object { $_.Name -eq 'hdi-headnode-private-pool' }
-    $privateLb | Add-AzureRmLoadBalancerRuleConfig -BackendAddressPool $headPoolObj -Probe $sshProbe -Name 'SSH' `
-        -FrontendIpConfiguration $frontendIPConfig -Protocol Tcp -FrontendPort 22 `
-        -BackendPort 22 -LoadDistribution SourceIPProtocol | Set-AzureRmLoadBalancer | Out-Null
 
-    Write-Output $privateLb
+    Write-Verbose 'Add Load Balancer Rule to allow SSH to the hdi-private-gateway'
+    $addLBRuleConfigParams = @{'LoadBalancer' = $privateLb
+                               'BackendAddressPool' = $headPoolObj
+                               'Probe' = $sshProbe
+                               'Name' = 'SSH'
+                               'FrontendIpConfiguration' = $frontendIPConfig
+                               'Protocol' = 'Tcp'
+                               'FrontendPort' = '22'
+                               'BackendPort' = '22'
+                               'LoadDistribution' = 'SourceIPProtocol'}
+    $lbRuleConfig = Add-AzureRmLoadBalancerRuleConfig @addLBRuleConfigParams
+    Write-Verbose 'Set the Load Balancer to allow SSH to the hdi-private-gateway'
+    $null = Set-AzureRmLoadBalancer -LoadBalancer $lbRuleConfig
 
+    Write-Verbose 'Loop through the headnode nics and add the backend address pool for hdi-headnode-private-pool'
     foreach ($nic in $headNics) {
         $nic.IpConfigurations[0].LoadBalancerBackendAddressPools.Add($headPool)
-        $nic | Set-AzureRmNetworkInterface | Out-Null
+        Write-Verbose "Setting load balancer backend address pool for network interface $($nic.Name)"
+        Write-Warning "Setting load balancer backend address pool for network interface $($nic.Name) can take a while. Please wait..."
+        $null = Set-AzureRmNetworkInterface -NetworkInterface $nic
     }
+
+    $privateLb
 }
---
+++ NewLoadBalancerForInternalHdiCluster.ps1
@@ -0,0 +1,100 @@
+﻿<#
+.NOTES
+	PowerShell controller script written to pass the proper information to Add-PrivateLoadBalancerToHDInsight function
+	Version 20180312.1
+	Written by Steven Judd on 2018/03/08
+	Updated by Steven Judd on 2018/01/12:
+        Added parameters for the HDI cluster information
+        Added help text
+    
+    Feature Requests:
+        tbd
+
+.SYNOPSIS
+    Script to create private load balancer for an HDI cluster
+.DESCRIPTION
+    Checks to ensure the Add-PrivateLoadBalancerToHDInsight function is loaded
+    and if not, tries to load it. The function must be in the same path as this
+    script. Then it checks for a connection to AzureRM and tries to connect if
+    it is not connected. Then it switches to the subscription specified in the
+    parameters. Finally it gets an object for the HDI cluster and passes that
+    object to the Add-PrivateLoadBalancerToHDInsight function.
+.LINK
+	https://github.com/01100010011001010110010101110000/AzureRmUtilities
+.PARAMETER ResourceGroupName
+    This parameter is the Resource Group of the HDI cluster
+    This parameter is required.
+.PARAMETER ClusterName
+    This parameter is the name of the HDI cluster
+    This parameter is required.
+.PARAMETER Subscription
+    This parameter is the subscription of the HDI cluster
+    This parameter is required.
+.EXAMPLE
+    NewLoadBalancerForInternalHdiCluster.ps1 -ResourceGroupName 'HDIRG' -ClusterName 'HDI01' -Subscription 'TestSub'
+    This command will create a private load balancer configuration for the HDI
+    cluster "HDI01" in the resource group "HDIRG" for the subscription "TestSub"
+#>
+
+
+param(
+    [Parameter(Position=0,Mandatory=$true)]
+        [string]$ResourceGroupName,
+    [Parameter(Position=1,Mandatory=$true)]
+        [string]$ClusterName,
+    [Parameter(Position=2,Mandatory=$true)]
+        [string]$Subscription
+)
+
+Write-Host "Checking the load of the Add-PrivateLoadBalancerToHDInsight function" -ForegroundColor Yellow
+if(-not(Get-Command Add-PrivateLoadBalancerToHDInsight -ErrorAction SilentlyContinue))
+{
+    try
+    {
+        . "$(Join-Path -Path $(Split-Path -Parent $PSCommandPath) -ChildPath 'Add-PrivateLoadBalancerToHDInsight.ps1')"
+        Write-Host "Confirming the load of the Add-PrivateLoadBalancerToHDInsight function" -ForegroundColor Yellow
+        Get-Command Add-PrivateLoadBalancerToHDInsight -ErrorAction Stop
+    }
+    catch
+    {
+        throw $_
+    }
+}
+
+#region check to see if connected to Azure and if not initiate a connection
+try
+{
+    Write-Host "Checking connection to AzureRM" -ForegroundColor Yellow
+    $AzureContext = Get-AzureRmContext -ErrorAction Stop
+    if ($AzureContext.Account.Id -notmatch $env:USERNAME)
+    {
+        throw "Run Add-AzureRmAccount to login to Azure" #this will move to the Catch block instead of throwing the error message
+    }
+}
+catch
+{
+    try
+    {
+        Write-Host "CONNECTING connection to AzureRM" -ForegroundColor Green
+        Add-AzureRmAccount -ErrorAction Stop
+        $AzureContext = Get-AzureRmContext -ErrorAction Stop
+    }
+    catch
+    {
+        throw $_
+    }
+}
+Write-Host "Azure Context: $($AzureContext.Tenant.Id)"
+#endregion
+
+Set-AzureRmContext -Subscription $Subscription
+try
+{
+    $cluster = Get-AzureRmHDInsightCluster -ResourceGroupName $ResourceGroupName -ClusterName $ClusterName
+    $results = Add-PrivateLoadBalancerToHDInsight -Cluster $cluster -Verbose
+    $results #| Select-Object Name,ResourceGroupName,Location,Id,Etag,ResourceGuid,ProvisioningState,Tags
+}
+catch
+{
+    throw $_
+}
